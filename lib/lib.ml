f0ea2475d45926bf44a6ab8d9503db177732593e
malatext <malatext@tiepi.it>
2026-05-15 20:39:54 +0200

    moved init_policy in policyChecker.ml

--- lib/lib.ml
+++ lib/lib.ml
@@ -8,17 +8,7 @@ module Parser = Parser
 module SymbolicInterpreter = SymbolicInterpreter
 module Utils = Utils
 module StrMap = Map.Make(String)
-open PolicyChecker
-
-(**)
-let init_policy serviceMap = function
-  | ContractAST.QosFieldOp (operator, aggregator, fieldName, i) (* meaning: <aggregator>(<fieldname>) <operator> i*)
-    -> {current = (match aggregator with
-                   | ContractAST.Sum | ContractAST.Avg -> 0
-                   | ContractAST.Min -> Int.max_int
-                   | ContractAST.Max -> Int.min_int); check = QosAggregate (operator, aggregator, fieldName, i)}
-  | ContractAST.Regex reg -> {current=0; check = Nfa ([](*list of final states*), (fun state service -> state) (*placeholder nfa, needs to be replaced by the one obtained by the regex2nfa conversion*))}
-  | ContractAST.Sort fieldName -> {current=0; check = Sorted fieldName}
+module PolicyChecker = PolicyChecker
                   
 let parse src =
   let lexbuf = Lexing.from_channel (open_in src) in
@@ -28,7 +18,7 @@ let symb_run ((contract : ContractAST.program), program) ~mode =
   (*create a map (name -> service)*)
   let serviceMap = List.fold_left (fun m (s : ContractAST.service) -> StrMap.add s.name s m) StrMap.empty contract.services
   in (*initialize each policy checker*)
-  let policyInitStates = List.map (init_policy serviceMap) contract.policies
+  let policyInitStates = List.map (PolicyChecker.init_policy serviceMap) contract.policies
   in
   (SymbolicInterpreter.build_symb_process program policyInitStates serviceMap)
   |> SymbolicInterpreter.Symex.run ~mode
--- lib/policyChecker.ml
+++ lib/policyChecker.ml
@@ -1,6 +1,17 @@
+module ContractAST = ContractAST
 type pCheck =
   | QosAggregate of ContractAST.binop * ContractAST.aggrop * string * int
-  | Nfa of int list * (int -> string -> int)
+  | Dfa of int list * (int -> string -> int)
   | Sorted of string
             
 type pChecker = {current : int; check : pCheck}
+
+(*the policy checker has a state that is updated at each invoke. If one update puts it in the final state, the policy is violated*)
+let init_policy serviceMap = function
+  | ContractAST.QosFieldOp (operator, aggregator, fieldName, i) (* meaning: <aggregator>(<fieldname>) <operator> i*)
+    -> {current = (match aggregator with
+                   | ContractAST.Sum | ContractAST.Avg -> 0
+                   | ContractAST.Min -> Int.max_int
+                   | ContractAST.Max -> Int.min_int); check = QosAggregate (operator, aggregator, fieldName, i)}
+  | ContractAST.Regex reg -> {current=0; check = Dfa ([](*list of final states*), (fun state service -> state) (*placeholder nfa, needs to be replaced by the one obtained by the regex2nfa conversion*))}
+  | ContractAST.Sort fieldName -> {current=0; check = Sorted fieldName}
