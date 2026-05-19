68fd953e0ad2044613747d497695d7d1dcd8a40e
malatext <malatext@tiepi.it>
2026-05-19 09:21:09 +0200

    QosAggregate and QosAvg now have a symbolic int in the state

--- lib/policyChecker.ml
+++ lib/policyChecker.ml
@@ -1,30 +1,29 @@
-type pCheck =
-  | QosAggregate of Contract.AST.binop * Contract.AST.aggrop * string * int
-  | Dfa of int list * (int -> string -> int)
-  | Sorted of string
+module Typed = Soteria.Tiny_values.Typed
+type symb_int = Typed.T.sint Typed.t
 
-type pChecker = { current : int; check : pCheck }
+type pChecker =
+  | QosAggregate of symb_int * Contract.AST.binop * Contract.AST.aggrop * string * int
+  | QosAvg of symb_int * int * Contract.AST.binop * string * int
+  | Dfa of int * (int -> string -> int) * int list
+  | Sorted of int * string (*Only works with constants, like thrust*)
 
 (*the policy checker has a state that is updated at each invoke. If one update puts it in the final state, the policy is violated*)
 let init_policy serviceMap = function
-  | Contract.AST.QosFieldOp (operator, aggregator, fieldName, i)
-  (* meaning: <aggregator>(<fieldname>) <operator> i*) ->
-      {
-        current =
-          (match aggregator with
-          | Contract.AST.Sum | Contract.AST.Avg -> 0
-          | Contract.AST.Min -> Int.max_int
-          | Contract.AST.Max -> Int.min_int);
-        check = QosAggregate (operator, aggregator, fieldName, i);
-      }
+  | Contract.AST.QosFieldOp (operator, Contract.AST.Avg, fieldName, i) ->
+     QosAvg (Typed.int 0, 0, operator, fieldName, i)
+  | Contract.AST.QosFieldOp (operator, aggregator, fieldName, i) ->
+     (* meaning: <aggregator>(<fieldname>) <operator> i *)
+     QosAggregate (
+         Typed.int (match aggregator with
+                    | Contract.AST.Sum | Contract.AST.Avg -> 0
+                    | Contract.AST.Min -> Int.max_int
+                    | Contract.AST.Max -> Int.min_int),
+         operator, aggregator, fieldName, i)
   | Contract.AST.Regex reg ->
-      {
-        current = 0;
-        check =
-          Dfa
-            ( [] (*list of final states*),
-              fun state service -> state
-              (*placeholder nfa, needs to be replaced by the one obtained by the regex2nfa conversion*)
-            );
-      }
-  | Contract.AST.Sort fieldName -> { current = 0; check = Sorted fieldName }
+     Dfa
+       ( 0,
+         (*placeholder nfa, needs to be replaced by the one obtained by the regex2nfa conversion*)
+         (fun state service -> state),
+         [] (*list of final states*)
+       )
+  | Contract.AST.Sort fieldName -> Sorted (0, fieldName)
