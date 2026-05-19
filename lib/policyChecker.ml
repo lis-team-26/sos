ed3d7a4e3580d0562e4f3f7f68f8b0bc3dde0a4a
malatext <malatext@tiepi.it>
2026-05-19 14:52:46 +0200

    made a clear distinction between grouped and ungrouped policies

--- lib/policyChecker.ml
+++ lib/policyChecker.ml
@@ -10,35 +10,34 @@ the parameter, has been assigned the same symbolic value (skip all invoked servi
  For 2), the group_by must be aware of the path-condition, so Soteria.Data.Map is used.
  For 1), just use a singleton 0 -> state *)
               
-
-type pCheck =
-  | QosAggregate of symb_int ValMap.t * Contract.AST.binop * Contract.AST.aggrop * string * int
-  | QosAvg of (symb_int * int) ValMap.t * Contract.AST.binop * string * int
-  | Dfa of int ValMap.t * (int -> string -> int) * int list
-  | Sorted of int ValMap.t * string (*Only works with constants, like thrust*)
-
-type pChecker = { check: pCheck; group_by: string option}
+type 'a checkerState =
+  | Ungrouped of 'a
+  | Grouped of string * 'a ValMap.t
+             
+type pChecker =
+  | QosAggregate of symb_int checkerState * Contract.AST.binop * Contract.AST.aggrop * string * int
+  | QosAvg of (symb_int * int) checkerState * Contract.AST.binop * string * int
+  | Dfa of int checkerState * (int -> string -> int) * int list
+  | Sorted of int checkerState * string (*Only works with constants, like thrust*)
             
 (*the policy checker has a state that is updated at each invoke. If one update puts it in the final state, the policy is violated*)
 let init_policy (policyType, _ (*for now, assume groupBy == None*)) =
-  { check =
       (match policyType with
        | Contract.AST.QosFieldOp (operator, Contract.AST.Avg, fieldName, i) ->
-          QosAvg (ValMap.singleton ((*Typed.int*) 0) ((Typed.int 0),0), operator, fieldName, i)
+          QosAvg ((Ungrouped ((Typed.int 0), 0)), operator, fieldName, i)
        | Contract.AST.QosFieldOp (operator, aggregator, fieldName, i) ->
           (* meaning: <aggregator>(<fieldname>) <operator> i *)
           QosAggregate (
-              ValMap.singleton ((*Typed.int*) 0) (Typed.int (match aggregator with
+              (Ungrouped (Typed.int (match aggregator with
                          | Contract.AST.Sum | Contract.AST.Avg -> 0
                          | Contract.AST.Min -> Int.max_int
-                         | Contract.AST.Max -> Int.min_int)),
+                         | Contract.AST.Max -> Int.min_int))),
               operator, aggregator, fieldName, i)
        | Contract.AST.Regex reg ->
           Dfa
-            ( ValMap.singleton ((*Typed.int*) 0) ((*Typed.int*) 0), (*current state*)
+            ( (Ungrouped 0), (*current state*)
               (*placeholder dfa, needs to be replaced by the one obtained by the regex2dfa conversion*)
               (fun state service -> state),
               [] (*list of final states*)
             )
-       | Contract.AST.Sort fieldName -> Sorted ((ValMap.singleton 0 0), fieldName));
-    group_by = None }
+       | Contract.AST.Sort fieldName -> Sorted ((Ungrouped 0), fieldName))
