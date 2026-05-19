632c98ac8c3a8ef96db64877523ce6e1b63c6201
dSamp41 <d.sampietro@studenti.unipi.it>
2026-05-19 15:10:41 +0200

    Move validate_contract

--- lib/contract.ml
+++ lib/contract.ml
@@ -16,3 +16,39 @@ let parse src =
     let parser = Parser.contract
   end) in
   Wrapper.parse src
+
+
+(* TODO: enforce invariants for contracts: 
+(v) no duplicate service names
+- regex use services that are defined in the program
+- policies only use services / QOS fields defined in the program
+- QoS constraints for each field of QoS vector
+*)
+
+module StringSet = Set.Make(String)
+
+let rec validate_regex regex service_names_set =
+  match regex with
+  | AST.RService s -> StringSet.mem s service_names_set
+  | AST.RConcat(r1, r2) | AST.RChoice (r1, r2) ->
+      validate_regex r1 service_names_set && validate_regex r2 service_names_set
+  | AST.RStar r -> validate_regex r service_names_set
+
+
+
+let validate_contract (contract: AST.contract) =
+  let service_names = List.map (fun (s: AST.service) -> s.name) contract.services in
+  let service_names_set = StringSet.of_list service_names in
+  
+  (* no duplicate service names *)
+  if List.length service_names <> StringSet.cardinal service_names_set then
+    failwith "Duplicate service names found in the contract";
+
+  (* bad prefix regex use services that are defined in the program*)
+  List.iter (fun (policy_type, _) ->
+    match policy_type with
+    | AST.Regex regex -> 
+        if not (validate_regex regex service_names_set) then
+          failwith "Regex in policy uses undefined service names"
+    | _ -> ()
+  ) contract.policies
