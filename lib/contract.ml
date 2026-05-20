955c58e5ab987cb120012a5ced64df5835eb754c
dSamp41 <d.sampietro@studenti.unipi.it>
2026-05-20 18:25:24 +0200

    Added invariant

--- lib/contract.ml
+++ lib/contract.ml
@@ -37,12 +37,20 @@ let rec validate_regex (regex: AST.regex) (service_names_set: StringSet.t) =
 
 
 let validate_contract (contract: AST.contract) =
+  
+  (* no duplicate service names *)
   let service_names = List.map (fun (s: AST.service) -> s.name) contract.services in
   let service_names_set = StringSet.of_list service_names in
   
-  (* no duplicate service names *)
   if List.length service_names <> StringSet.cardinal service_names_set then
     failwith "Duplicate service names found in the contract";
+
+  (* no duplicate QoS fields *)
+  let qos_fields = List.map fst contract.qos in
+  let qos_fields_set = StringSet.of_list qos_fields in
+
+  if List.length qos_fields <> StringSet.cardinal qos_fields_set then
+    failwith "Duplicate QoS fields found in the contract";
 
   List.iter (fun (policy_type, _) ->
     match policy_type with
@@ -56,4 +64,37 @@ let validate_contract (contract: AST.contract) =
         if not (List.mem field qos_fields) then
           failwith ("QoS policy uses undefined QoS field: " ^ field) 
     | AST.Sort _ -> ()
-  ) contract.policies
+  ) contract.policies;
+
+  (* QoS constraints for each field of QoS vector *)
+  (*
+  - \forall service: \forall qos_field: \exists constraint
+  *)
+  List.iter (fun (service: AST.service) ->
+    let effect_vars = List.fold_left (fun acc (lhs, _) ->
+      match lhs with
+      | AST.LVar v -> StringSet.add v acc
+      | AST.LApp (f, args) ->
+          List.fold_left (fun acc arg ->
+            match arg with
+            | AST.EVar v -> StringSet.add v acc
+            | _ -> acc
+          ) acc args
+    ) StringSet.empty (fst service.qos) in
+    let constrnt_fields = List.fold_left (fun acc (binop, lhs, expr) ->
+      match lhs with
+      | AST.LVar v -> StringSet.add v acc
+      | AST.LApp (f, args) ->
+          List.fold_left (fun acc arg ->
+            match arg with
+            | AST.EVar v -> StringSet.add v acc
+            | _ -> acc
+          ) acc args
+    ) StringSet.empty (snd service.qos) in
+    let qos_constraints = StringSet.union effect_vars constrnt_fields in
+    if not (StringSet.equal qos_constraints qos_fields_set) then
+      let missing_fields = StringSet.diff qos_fields_set qos_constraints in
+      let missing_fields_str = String.concat ", " (StringSet.elements missing_fields) in
+      failwith ("Service " ^ service.name ^ " is missing QoS constraints:for the field(s): " ^ missing_fields_str)
+  ) contract.services
+
